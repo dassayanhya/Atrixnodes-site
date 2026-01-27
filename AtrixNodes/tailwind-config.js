f255938ed1706a420480015c8c3e800e3907d3e7
Sayanhya Das <155005982+dassayanhya@users.noreply.github.com>
2026-01-28 01:24:09 +0530

    Initial Commit

---
+++ AtrixNodes/tailwind-config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+            colors: {
+                brand: {
+                    50: '#ecfeff',
+                    100: '#cffafe',
+                    400: '#22d3ee', // Cyan
+                    500: '#06b6d4',
+                    600: '#0891b2',
+                    900: '#0f172a', // Slate 900
+                    950: '#020617',
+                }
+            }
+        }
+    }
+}
